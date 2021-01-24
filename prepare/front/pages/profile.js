f4de9f18a41d5a20d7a871468dd1627a7fc8db87
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-24 22:06:11 +0900

    antd와 styled-components

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
 const profile = () => {
   return (
-    <AppLayout>
-      <div>프로필 페이지!</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>프로필 페이지!</div>
+      </AppLayout>
+    </>
   );
 };
 
