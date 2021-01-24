47d58b90e8ca931e43d9bfdba6147849857c4d32
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-24 12:57:10 +0900

    Setting

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
-	return <div>Hello, NEXT!</div>;
+  return (
+    <AppLayout>
+      <div>Hello, NEXT!</div>
+    </AppLayout>
+  );
 };
 
 export default Home;
