96304b8ff4098899c815f32cfd913aa35aeac60a
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-25 22:44:25 +0900

    antd 반응형 그리드

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
@@ -18,11 +18,27 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="http://www.naver.com" target="_blank" rel="noreferrer noopener">
+            Naver
+          </a>
+        </Col>
+      </Row>
       {children}
     </div>
   );
