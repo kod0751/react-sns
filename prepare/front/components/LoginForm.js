9d8a3533a132aa57b8ff429d2a5846d48741f129
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-27 22:15:42 +0900

    더미데이터 로그인 및 로그아웃

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -7,7 +7,11 @@ const ButtonWrapper = styled.div`
   margin-top: 10;
 `;
 
-const LoginForm = () => {
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
 
@@ -16,11 +20,16 @@ const LoginForm = () => {
   }, []);
 
   const onChangePassword = useCallback((e) => {
-    setId(e.target.password);
+    setPassword(e.target.password);
   }, []);
 
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password);
+    setIsLoggedIn(true);
+  }, [id, password]);
+
   return (
-    <Form>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
@@ -41,7 +50,7 @@ const LoginForm = () => {
           </a>
         </Link>
       </ButtonWrapper>
-    </Form>
+    </FormWrapper>
   );
 };
 
