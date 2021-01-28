fc965b8dd61a67a45a9cb6819498a14e466cf178
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-01-28 22:17:25 +0900

    프로필 페이지 만들기

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const profile = () => {
+  const followerList = [{ nickname: 'nick' }, { nickname: 'name' }, { nickname: 'Id' }];
+  const followingList = [{ nickname: 'nick' }, { nickname: 'name' }, { nickname: 'Id' }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>프로필 페이지!</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
