7ecc4216ce20b0d9d132bffbc0f85a407a18acff
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-02-13 15:12:06 +0900

    이미지 사이즈, 구조 수정

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -19,15 +19,15 @@ const PostImages = ({ images }) => {
   if (images.length === 2) {
     return (
       <>
-        <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom} />
-        <img role="presentation" width="50%" src={images[1].src} alt={images[1].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={images[1].src} alt={images[1].src} onClick={onZoom} />
       </>
     );
   }
   return (
     <>
       <div>
-        <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%' }} src={images[0].src} alt={images[0].src} onClick={onZoom} />
         <div role="presentation" style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }} onClick={onZoom}>
           <PlusOutlined />
           <br />
