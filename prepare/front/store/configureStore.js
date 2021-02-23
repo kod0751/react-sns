765a9c8d6768fc676fefde53fa0d821973b0d030
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-02-24 00:13:54 +0900

    netx-redux-saga

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, createStore, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from '../reducers';
+import rootSaga from '../Sagas';
+
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+};
 
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer = process.env.NODE_ENV === 'production' ? compose(applyMiddleware(...middlewares)) : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
